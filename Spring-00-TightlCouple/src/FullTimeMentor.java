a2335f61e2aaf14a2231653b012ba1aa9f214bcb
russell495 <rukovalchuk3@gmail.com>
2021-09-21 23:17:57 -0400

    firstCommits

---
+++ Spring-00-TightlCouple/src/FullTimeMentor.java
@@ -0,0 +1,6 @@
+public class FullTimeMentor {
+
+    public void createAccount(){
+        System.out.println("FullTime mentor account is created..");
+    }
+}
---
+++ Spring-00-TightlCouple/src/PartTimeMentor.java
@@ -0,0 +1,6 @@
+public class PartTimeMentor {
+
+    public void createAccount(){
+        System.out.println("PartTime mentor account is created..");
+    }
+}
